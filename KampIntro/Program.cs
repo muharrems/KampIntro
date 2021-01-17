09ddeaba51b418a9fdafb52e54c05086731032ba
Muharrem Sari <m_saritr@hotmail.com>
2021-01-18 01:44:23 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int deger = 12;
+            bool isOk = false;
+            if (isOk)
+            {
+                Console.WriteLine("Giriş ok");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yapılmadı");
+            }
+        }
+    }
+}
